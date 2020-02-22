fd846fc238bdce77eacd64c35b3eb2311078f849
Jacob <lcs1998@vip.qq.com>
2020-02-22 23:00:49 +0800

    :tada: 项目初始化

---
+++ app/src/main/java/com/example/awesomecurriculum/ui/chat/ChatViewModel.java
@@ -0,0 +1,19 @@
+package com.example.awesomecurriculum.ui.chat;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ChatViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ChatViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/example/awesomecurriculum/ui/schedule/ScheduleViewModel.java
@@ -0,0 +1,19 @@
+package com.example.awesomecurriculum.ui.schedule;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ScheduleViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ScheduleViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
