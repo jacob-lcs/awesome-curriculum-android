77dd188724ec96bcb4d452d7ab36f98c0d352d16
Jacob <lcs1998@vip.qq.com>
2020-03-08 11:48:11 +0800

    :fire: 去除tabbar

--- app/src/main/java/com/example/awesomecurriculum/MainActivity.java
+++ app/src/main/java/com/example/awesomecurriculum/MainActivity.java
@@ -5,21 +5,14 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
 
 import com.example.awesomecurriculum.ui.login.LoginActivity;
 import com.example.awesomecurriculum.utils.DatabaseHelper;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
+/**
+ * @author Jacob
+ */
 public class MainActivity extends AppCompatActivity {
 
     public DatabaseHelper databaseHelper = new DatabaseHelper
@@ -29,18 +22,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_schedule, R.id.navigation_chat)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(navView, navController);
         if (!logined()) {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
             finish();
         }
-//        setCustomActionBar();
     }
 
     /**
@@ -54,21 +40,6 @@ public class MainActivity extends AppCompatActivity {
         boolean result = cursor.getCount() != 0;
         cursor.close();
         return result;
-    }
-
-    /**
-     * 自定义toolbar
-     */
-    private void setCustomActionBar() {
-        ActionBar.LayoutParams lp = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT, Gravity.CENTER);
-        View mActionBarView = LayoutInflater.from(this).inflate(R.layout.layout_actionbar, null);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setCustomView(mActionBarView, lp);
-        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        actionBar.setDisplayShowCustomEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(false);
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setElevation(0);
     }
 
 }
